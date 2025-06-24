fcf6cc3415a86fa07a819c58bad6742b49fca76b
Anurag kumar <anuragkumar1252003@gmail.com>
2025-06-25 00:11:40 +0530

    first python code containing print statements, escape sequence, seperators

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("hey mu naamo anurag auchi") #simple print statement 
+print("hey mu naamo \'anurag\' auchi") #escape sequence '\'
+print("hey my name is \'Anurag\',\nI am a post-graduate student") #line breaker using '\n'
+print("hey with",69,"others", sep="~") #output = hey with~69~others , sep= seperator.   
