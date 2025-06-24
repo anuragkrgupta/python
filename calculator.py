7bdde82c229be4e8b7b84e9452e8ff5c62ca1ced
Anurag kumar <anuragkumar1252003@gmail.com>
2025-06-25 01:21:42 +0530

    syntax update

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
 # A dynamic calculator using python which calculates all the basic mathematics calculation
 
-user_name = str(input("hey user what is your name "))
-print("hello", user_name, "welcome to python calculator")
+user_name = str(input("Hey user what is your name "))
+print("Hello", user_name, "welcome to python calculator")
 
 value1 = float(input("Please enter your first number "))
 value2 = float(input("Please enter your second number "))
-sign = input("please enter the operation type.. ex:- *, /, +, -, **, //, %  ")
+sign = input("Please enter the operation type..  ")
 
 if sign == '+':
     result = value1 + value2
