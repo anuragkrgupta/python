f7e370217aae8387f21e62212062e87e9496236f
Anurag kumar <anuragkumar1252003@gmail.com>
2025-06-30 03:58:48 +0530

    Add function implementations for addition, division, and prime checking

---
+++ function.py
@@ -0,0 +1,26 @@
+# def calculate (a, b):  #we are creating a function here named calculate
+#     add = a+b
+#     print(add)
+
+# a=9     
+# b=9
+# calculate (a, b)        #here we are calling the function
+
+
+# def divide(a,b):
+#     div = a/b
+#     print(div)
+
+# a = int(input("enter your 1st number "))
+# b = int(input("enter your 2nd number "))
+
+# divide(a,b)
+
+# def isPrime(a):   # checking prime number dynamically!!!!
+#     if a%2 == 0:
+#         print(a," is a prime number.")
+#     else: 
+#         print(a," is not a prime number.")
+
+# a = int(input("enter a number "))
+# isPrime(a)
