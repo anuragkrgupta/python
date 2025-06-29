6f67011fc8991eef9aa1ca16c9b0f7d5f3cdcf74
Anurag kumar <anuragkumar1252003@gmail.com>
2025-06-29 16:39:54 +0530

    Add initial implementation of loop examples in Python

---
+++ loops.py
@@ -0,0 +1,28 @@
+#here we are exploring loops
+
+#for loop
+# name = "anurag"
+
+# for i in name:  #here we are printing each characters of a string
+    # print(i)
+
+# list = ["aluu", "pyazz", "tamatar"]
+
+# for items in list: #here we are printing items of the list
+#     print(items)
+
+# range 
+
+# for n in range(1, 5): #it will print from 1 to 4
+    # print(n) 
+
+# for n in range (0, 21, 4): # this is third parameter of range, here starts at 0, stops before 21 (exclusive), and increments by 4 for each number in the sequence
+    # print(n)
+
+#while loop
+
+# i = 1
+# while(i<=3):
+#     print(i)
+#     i = i+1
+# else: print("loop isn't work") #this will print if the condition will not work or it will come out of the loop
