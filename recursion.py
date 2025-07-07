f2d7b2e932a62f23bace13949e973aad1cd650b9
Anurag kumar <anuragkumar1252003@gmail.com>
2025-07-08 03:52:38 +0530

    Refactor recursion.py to remove unnecessary whitespace and ensure consistent formatting; add Fibonacci function implementation.

--- recursion.py
+++ recursion.py
@@ -2,7 +2,6 @@
 
 #python recursion function
 #factorial
-
 def factorial(n):  # here we are creating a function
     if (n==0) or (n==1):  #checking the condition
         return 1  #if the condition is true, return 1
@@ -10,3 +9,17 @@ def factorial(n):  # here we are creating a function
         return n * factorial(n-1)  #else it will perform this calculation
 print(factorial(5))  #calling the function. 
 
+#fibonacci series using recurrsion method
+def fibo(f):
+    if f<=1:  
+        return f
+    else:
+        return fibo(f-1) + fibo(f-2)
+
+term = int(input("enter the range: "))
+
+print("fibonaci series: ")
+
+for i in range(term):
+     print(fibo(i), end = ' ')
+ 
