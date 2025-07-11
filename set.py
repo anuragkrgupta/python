d09268877cd6ec56b38e3749f6f9806f903cb09f
Anurag kumar <anuragkumar1252003@gmail.com>
2025-07-12 02:24:42 +0530

    Add initial implementation of set demonstration with examples

---
+++ set.py
@@ -0,0 +1,11 @@
+#lets talk about set
+#set is a unordered collection of data items, they store multiple items in a single variable , sets are unchangeable
+
+s = {1, 2, 8, 9, 9}  
+print(s) # {8, 1, 2, 9} as sets is a collection of well define object
+
+info = {19, 21, "anurag", True}
+print(info)  #{True, 19, 21, 'anurag'}   set is a unordered collection of data items
+
+prakar = set() #empty set
+print(type(prakar))  #<class 'set'>  
