46c5b87cc64b9ba2061c10fd8d56dd9b0089a834
Anurag kumar <anuragkumar1252003@gmail.com>
2025-07-20 01:20:06 +0530

    Add encryption and decryption rules documentation

---
+++ encryption_decryption.py
@@ -0,0 +1,11 @@
+#here we will encrypt and decrypt some message using python code
+#lets begin
+
+#rules for encrytion:-
+#if the word is >=3 then reverse the order of characters
+#if word is <3 then remove the 1st character and append it to the last,
+#now add 3 random characters to the starting and ending of the word.
+
+#rules for decryption:-
+#reverse all the task performed for encrypting the message.
+
