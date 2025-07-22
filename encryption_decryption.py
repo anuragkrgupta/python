743157653002d2e82b70e0c54585fd67fbfa7d4b
Anurag kumar <anuragkumar1252003@gmail.com>
2025-07-22 23:58:30 +0530

    Refactor code structure for clarity and maintainability

--- encryption_decryption.py
+++ encryption_decryption.py
@@ -8,34 +8,36 @@
 
 #rules for decryption:-
 #reverse all the task performed for encrypting the message.
-import random
-import string
 
-msglen = ()
-encrypted_db = {}
+
+import random  #for generating random things
+import string  #for random string
+
+msglen = ()  #storing message lenth here
+encrypted_db = {}   #database
 
 
 msg_input = input("Type your message here:- ")
-while True:
+while True:  #loop for multiple inputs
     req = input("what you want to do?\n press 'e' for encryption & 'd' for decryption Or press 'q' for exit!!")
 
     if req.lower() == "e":
-        ran_letters = ''.join(random.choices(string.ascii_letters, k=4))     
-        msglen = len(msg_input)
+        ran_letters = ''.join(random.choices(string.ascii_letters, k=4))   #generating random letter   
+        msglen = len(msg_input)  #calculating the length of the message
         # print(msglen)
         if msglen<=3:
-            reverse_s = msg_input[::-1]
+            reverse_s = msg_input[::-1]  #reversing the string
             print(reverse_s)
         print("encrypted")
         if msglen>3:
-            rotate = msg_input[1:] + msg_input[0]
+            rotate = msg_input[1:] + msg_input[0]  #appending the 1st char into last
             # print(rotate)
-            encrypted = ran_letters + rotate + ran_letters
-            encrypted_db[msg_input] = encrypted
+            encrypted = ran_letters + rotate + ran_letters  #adding 4 random char 
+            encrypted_db[msg_input] = encrypted  #storing the output to a database
             print(encrypted)
     elif req == "d":
         # print(encrypted)
-        
+
         print("decrypted")
     else:
         break
