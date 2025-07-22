65e0203d27bf98d6a43098b95d58d588ebae4fc3
Anurag kumar <anuragkumar1252003@gmail.com>
2025-07-22 18:50:24 +0530

    ///

--- encryption_decryption.py
+++ encryption_decryption.py
@@ -2,8 +2,8 @@
 #lets begin
 
 #rules for encrytion:-
-#if the word is >=3 then reverse the order of characters
-#if word is <3 then remove the 1st character and append it to the last,
+#if the word is <=3 then reverse the order of characters
+#if word is >3 then remove the 1st character and append it to the last,
 #now add 3 random characters to the starting and ending of the word.
 
 #rules for decryption:-
