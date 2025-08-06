9c3bc5648270d6d5e8853a1979226b29e4d88e98
Anurag kumar <anuragkumar1252003@gmail.com>
2025-08-06 20:42:51 +0530

    Add comments to clarify recursion examples and structure

---
+++ following_output_given_input.py
@@ -0,0 +1,2 @@
+#write a program where the input will be executed and printed as followes 
+#for example:- input[2a3(bc)] ; output[aabcbcbc] ; input[3a2bc] ; output[abcabca]
--- recursion.py
+++ recursion.py
@@ -1,25 +1,28 @@
-#recursion is the process of defining something in term of itself
+# #recursion is the process of defining something in term of itself
 
-#python recursion function
-#factorial
-def factorial(n):  # here we are creating a function
-    if (n==0) or (n==1):  #checking the condition
-        return 1  #if the condition is true, return 1
-    else:  
-        return n * factorial(n-1)  #else it will perform this calculation
-print(factorial(5))  #calling the function. 
+# #python recursion function
+# #factorial
+# def factorial(n):  # here we are creating a function
+#     if (n==0) or (n==1):  #checking the condition
+#         return 1  #if the condition is true, return 1
+#     else:  
+#         return n * factorial(n-1)  #else it will perform this calculation
+# print(factorial(5))  #calling the function. 
 
-#fibonacci series using recurrsion method
-def fibo(f):
-    if f<=1:  
-        return f
-    else:
-        return fibo(f-1) + fibo(f-2)
+# #fibonacci series using recurrsion method
+# def fibo(f):
+#     if f<=1:  
+#         return f
+#     else:
+#         return fibo(f-1) + fibo(f-2)
 
-term = int(input("enter the range: "))
+# term = int(input("enter the range: "))
 
-print("fibonaci series: ")
+# print("fibonaci series: ")
 
-for i in range(term):
-     print(fibo(i), end = ' ')
- 
+# for i in range(term):
+#      print(fibo(i), end = ' ')
+
+#star lader pattern
+
+
